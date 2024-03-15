a4782903ab381b006d84d82698d655660d6b4e61
niket_panjwani@mckinsey.com <niket_panjwani@mckinsey.com>
2024-03-15 04:47:42 -0500

    Initializing chat panel and react app for its ui

---
+++ webview-ui/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx', // The entry point of your React app
+  output: {
+    path: path.resolve(__dirname, '../out'), // The output directory
+    filename: 'bundle.js', // The name of the output file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+};
